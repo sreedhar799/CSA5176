7fe81a826f28972c15b3a47676a3752f1e6ce6a8
sreedhar799 <133748616+sreedhar799@users.noreply.github.com>
2023-05-17 10:18:42 +0530

    Create 32 DSA RSA.c

---
+++ 32 DSA RSA.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int num;
+
+    srand(time(0));
+
+    num = rand() % 100;
+
+    printf("Random number: %d\n", num);
+
+    return 0;
+}
